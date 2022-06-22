72dcd8f749a4948edcc5eb6c0650e68e2d6ce121
Binu2 <108000997+Binu2@users.noreply.github.com>
2022-06-22 17:55:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94740855698']
+global.premium = ['94740855698']
+global.ownernomer = '94740855698'
+global.ownername = '𝚓𝚎𝚛𝚛𝚢 𝚖𝚍🍁'
+global.botname = '𝚓𝚎𝚛𝚛𝚢 𝚖𝚍🍁'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = 'https://chat.whatsapp.com/EmBzcyhWNAwLyKNFkv6EGS'
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = '𝚓𝚎𝚛𝚛𝚢 𝚖𝚍🍁'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
